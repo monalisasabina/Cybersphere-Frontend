d45b08abeb93a98da915ccc33e71a5a3cf0ccbe8
Monalisa Sabina <onyangomonalisa@hotmail.com>
2025-07-24 19:31:18 +0300

    working on signup 1

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,5 +1,6 @@
 import { useRef, useState } from "react";
 import "./SignUp.css"
+import { data } from "react-router-dom";
 
 function SignUp(){
 
@@ -14,13 +15,72 @@ function SignUp(){
 
   const fileInputRef = useRef()
 
-  // Handle Box Click_______________________________________________________________
+   // Handle Box Click________________________________________________________________________
    const handleBoxClick = () => {
          fileInputRef.current.click();
    }
 
+   // Handle file selection______________________________________________________________________
+   const handleFileChange = (event) => {
+      
+      const file = event.target.files[0];
 
-   
+      if (file) {
+            setProfilePic(file);
+            setPreview(URL.createObjectURL(file))
+      }
+   };
+
+
+    //Hanfle submit form__________________________________________________________________________
+    const  handleSubmit = async (event) => {
+
+      event.preventDefault();
+
+      // Signing up the user
+      const response = await fetch("http://127.0.0.1:5555/signup", {
+            method: "POST",
+            headers: {
+                  "Content-Type":" application/json"
+            },
+            body: JSON.stringify({
+                  firstname: firstName,
+                  lastName: lastName,
+                  username,
+                  email,
+                  password,
+                  confirm_password: confirmPassword
+            }),
+      });
+
+      const data = await response.json();
+
+      if(response.ok){
+         const token =data.access_token;
+      }
+
+      // Uploading photo
+      if (profilePic) {
+
+            const formData = new FormData();
+            formData.append("image", profilePic);
+
+            const uploadRes = await fetch("http://127.0.0.1:5555/upload", {
+                  method:"POST",
+                  // headers: {
+                  //       Authorization: `Bearer ${token}}`
+                  // },
+                  body: formData,
+            });
+
+            const uploadData = await uploadRes.json();
+            console.log("Upload response", uploadData);
+
+      } else {
+            console.log("Signup failed", data)
+      }
+    };    
+
 
   return (
     <div>
@@ -28,7 +88,7 @@ function SignUp(){
       <h2>Sign Up Page</h2>
 
       <div>
-          <form>
+          <form onSubmit={handleSubmit}>
             {/* <input 
                   name="profile_pic"
                   type="file"
@@ -36,13 +96,21 @@ function SignUp(){
                   onChange={(event) => setProfilePic(event.target.value)}
             /> */}
 
-            <div 
-                 className="box_click"
-                 
-                 
-                 >
-
+            <div className="box_click" onClick={handleBoxClick}>
+                  {preview ? (
+                        <img src={preview} alt="Profile pic preview" style={{ width: "100px"}}/>
+                  ) : (
+                        <p>Click to select profile</p>
+                  )}
             </div>
+
+            <input 
+                  type="file"
+                  ref={fileInputRef}
+                  style={{ display: "none"}}
+                  accept="image/*"
+                  onChange={handleFileChange}
+            />
 
             <input
                   name="firstname"
@@ -64,28 +132,19 @@ function SignUp(){
 
             <input
                   name="username"
-                  type="email"
-                  value={email}
-                  placeholder="Enter your Email Address"
-                  onChange={(event) => setEmail(event.target.value) }
-                  required
-            />
-
-            <input
-                  name="username"
-                  type="email"
-                  value={email}
-                  placeholder="Enter your Email Address"
-                  onChange={(event) => setEmail(event.target.value) }
-                  required
-            />
-
-             <input
-                  name="email"
                   type="text"
                   value={username}
                   placeholder="Enter your username"
                   onChange={(event) => setUserName(event.target.value) }
+                  required
+            />
+
+            <input
+                  name="email"
+                  type="email"
+                  value={email}
+                  placeholder="Enter your email"
+                  onChange={(event) => setEmail(event.target.value) }
                   required
             />
 
@@ -107,7 +166,7 @@ function SignUp(){
                   required
             />
 
-
+            <button type="submit">ADD USER</button>
           </form>
       </div>
 
